29fa74846beaaa88ba75d325f56512de8f65f23a
NguyenPhThao23174600016 <148135338+NguyenPhThao23174600016@users.noreply.github.com>
2023-12-31 12:53:35 +0700

    Delete Bai_8.5.py

--- Bai_8.5.py
+++
@@ -1,6 +0,0 @@
-#Bai_8.5:Kiem tra nam nhuan
-a = eval(input("Nhap year: "))
-if ((a%4==0) and (a%100!=0) or (a%400==0)):
-    print("nam", a, "la nam nhuan")
-else:
-    print("nam", a, "khong phai nam nhuan")
