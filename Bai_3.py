89b7443b1e8d2bafd73b85ecab1285db47879698
NguyenPhThao23174600016 <148135338+NguyenPhThao23174600016@users.noreply.github.com>
2023-12-31 12:30:07 +0700

    Delete Bai_3.py

--- Bai_3.py
+++
@@ -1,8 +0,0 @@
-#Viet chuong tirnh tinh tien phai tra khi mua hang
-ten_hang="hop sua vnia milk"
-so_luong=5
-don_gia=25000
-so_tien_phai_tra=so_luong*don_gia
-print("ten hang", ":" , ten_hang)
-print("so luong", ":" , so_luong)
-print("so tien phai tra", ":" , so_tien_phai_tra)
