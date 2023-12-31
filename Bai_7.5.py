e46d3f024b5016b16a82a933f91205037392b926
NguyenPhThao23174600016 <148135338+NguyenPhThao23174600016@users.noreply.github.com>
2023-12-31 12:28:49 +0700

    chuong 7

---
+++ Bai_7.5.py
@@ -0,0 +1,10 @@
+#Bai_7.5:Tinh ket qua 5
+x = 15
+y = 12
+print('Binary of x =', bin(x), ', Binary of y =', bin(y))
+print('x & y =', bin(x & y))
+print('x | y =', bin(x | y))
+print('x ^ y =', bin(x ^ y))
+print('~x =', bin(~x))
+print('x << 2 =', bin(x << 2))
+print('y >>2 =', bin(y >>2))
